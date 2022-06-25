d52b75215851e23fd381e0e44775b952af359f36
prashant <prashantarjariya1234@gmail.com>
2022-06-26 01:19:37 +0530

    Added Firebase setup and Header Component

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
 
+
+
+
+import React, { Component } from 'react';
+import './App.css';
+import Header from './Components/Header';
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      {<Header />}
+      {/*Home */}
     </div>
   );
 }
